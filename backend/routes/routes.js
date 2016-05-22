0055ed6432152bfcb6ad76ec8549ee61f370bf5e
Allan Lund Hansen <allan@dropz.com>
2016-05-22 18:27:15 +0800

    adding the base64 to the resize file for now

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,18 +7,34 @@ const appRouter = function(app) {
     const srcDir = path.resolve(baseDir, 'web');
 
     const gm = require('gm');
+    const base64 = require('node-base64-image');
+    const options = {localFile: true, string: true};
+
+    function extractBase64 (img, fileExtension) {
+
+      base64.base64encoder(img, options, function (err, image) {
+        if (err) {
+          console.log(err);
+        }
+        const base64ImgString = 'data:image/' + fileExtension + ';base64,';
+        console.log(base64ImgString + image);
+      });
+
+    }
 
     // =TODO= Get argument
 
     function createThumbnail() {
       const img = gm(req.query.fileName);
+      const fileExtension = path.extname(req.query.fileName).split('.').pop();
 
       img.resize(40);
       img.noProfile();
-      img.write('test.jpg', (err) => {
+      img.write('test.' + fileExtension, (err) => {
         if (err) {
           console.log(err)
         }
+        extractBase64('test.' + fileExtension, fileExtension);
       });
     }
 
